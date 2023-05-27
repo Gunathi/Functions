e85beea54f01f1b8d7592f409f6c975bb26c9b4a
Gunathi <hansinima20003@gmail.com>
2023-05-27 10:06:17 +0530

    simple calculater

--- functions.js
+++ functions.js
@@ -1,4 +1,41 @@
+//creating a function;
+
 function remain(a,b) {
     return a%b;
 }
 console.log(remain(10,3)); //1
+
+function add(x,y) {
+    return x+y;
+}
+
+function sub(x,y) {
+    return x-y;
+}
+
+function div(x,y) {
+    return x/y;
+}
+
+function mul(x,y) {
+    return x*y;
+}
+
+console.log(add(2,5)); //7
+console.log(sub(16,4)); //12
+console.log(div(16,6)); //2.6666666666666665
+console.log(mul(2*4)); //
+
+
+function calculator(x,y,operator) {
+    if(operator == '+'){
+        return x+y;
+    }else if(operator == '-') {
+        return x-y;
+    }else if(operator == '/') {
+        return x/y;
+    }else if(operator == '*') {
+        return x*y;
+    }
+}
+    console.log(calculator(2,5,'+')); //7
